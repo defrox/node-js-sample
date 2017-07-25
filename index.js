21e8edcbea7cff4f18ba37c61f9c5f8d422b3140
Christian Schmidt <christian.schmidt@tmceurope.com>
2017-07-25 11:46:45 +0200

    Second commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    response.send('Hello World from BackSpace Lab!”')
 })
 
 app.listen(app.get('port'), function() {
